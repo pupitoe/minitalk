5d283cdcea60e23e4b2bc27834ccdecf2f59734b
Tom Lasseret <tlassere@k1r1p4.42mulhouse.fr>
2024-01-04 13:23:56 +0100

    fix bugs, the string is push in one try

--- dserver/main.c
+++ dserver/main.c
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 10:02:12 by tlassere          #+#    #+#             */
-/*   Updated: 2024/01/04 02:59:21 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/01/04 13:23:17 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,29 +26,29 @@ static pid_t	ft_print_pid(void)
 
 static int	ft_rep_signal(int signal, int delet)
 {
-	static size_t	i = 0;
-	static char		c = 0;
+	static int		i = 0;
+	static size_t	size = 0;
 	static char		*buffer = NULL;
 
-	if (signal == SIGUSR1)
-		c = c | 1 << i % 8;
-	i++;
-	if (i % 8 == 0)
+	if (i == 0)
 	{
-		if (ft_add_car(&buffer, c, i / 8) == -1)
+		size++;
+		if (ft_add_car(&buffer, 0, size) == -1)
 			return (-1);
-		if (buffer[i / 8] == '\0')
-			ft_putstr(buffer);
-		c = 0;
 	}
-	if ((buffer && buffer[i / 8] == '\0') || delet)
+	if (signal == SIGUSR1)
+		buffer[size - 1] = buffer[size - 1] | 1 << i % 8;
+	if (++i == 8)
+		i = 0;
+	if (buffer[size - 1] == '\0' && i == 0)
+		ft_putstr(buffer);
+	if ((buffer[size - 1] == '\0' && i == 0) || delet)
 	{
 		free(buffer);
 		buffer = NULL;
+		size = 0;
 		i = 0;
 	}
-	if (delet)
-		c = 0;
 	return (0);
 }
 
@@ -71,7 +71,8 @@ static void	ft_get_signal(int signal, siginfo_t *info, void *ucontext)
 
 int	main(void)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
+
 	ft_print_pid();
 	ft_memset(&sa, 0, sizeof(struct sigaction));
 	sa.sa_flags = SA_SIGINFO;
