0d8a84c1baf735894b2cd1999277af1f31d0ccf9
Tom Lasseret <tlassere@k1r1p3.42mulhouse.fr>
2024-01-06 19:33:31 +0100

    sigaction fail quit programme

--- dserver/main.c
+++ dserver/main.c
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 10:02:12 by tlassere          #+#    #+#             */
-/*   Updated: 2024/01/06 17:54:33 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/01/06 19:30:34 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,10 @@ int	main(void)
 	ft_memset(&sa, 0, sizeof(struct sigaction));
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = &ft_get_signal;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		return (1);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		return (1);
 	while (1)
 	{
 		sleep(5);
