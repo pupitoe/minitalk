262b40be052e8392c7aacabc633fe11ea4f10cd5
Tom Lasseret <tlassere@k1r1p6.42mulhouse.fr>
2024-01-07 17:23:59 +0100

    change name function

--- commun/ft_put_string2.c
+++ commun/ft_put_string2.c
@@ -6,13 +6,13 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 13:47:29 by tlassere          #+#    #+#             */
-/*   Updated: 2024/01/03 13:47:49 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/01/07 17:19:28 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commun.h"
 
-char	*ft_copy_at(char *str, size_t size, int c)
+char	*ft_add_car_str(char *str, size_t size, int c)
 {
 	char	*buffer;
 	size_t	i;
@@ -35,7 +35,7 @@ int	ft_add_car(char **str, int c, size_t size)
 	char	*buffer;
 
 	buffer = *str;
-	*str = ft_copy_at(buffer, size, c);
+	*str = ft_add_car_str(buffer, size, c);
 	if (buffer != NULL)
 		free(buffer);
 	if (*str == NULL)
